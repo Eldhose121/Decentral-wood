470c8d065fb5ec47efbf60cda1dc962051170482
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-11-06 18:02:25 +0530

    Integrated web3

--- src/components/App.js
+++ src/components/App.js
@@ -27,12 +27,77 @@ class App extends Component {
       window.alert('Non-Ethereum browser detected. You should use the MetaMask extension!')
     }
   }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = DMRforum.networks[networkId]
+    if(networkData) {
+      const dmr = web3.eth.Contract(DMRforum.abi, networkData.address)
+      this.setState({ dmr })
+      const rCount = await dmr.methods.rCount().call()
+      this.setState({ dmr })
+      for (var i = 1; i <= rCount; i++) {
+        const review = await dmr.methods.reviews(i).call()
+        this.setState({
+          reviews: [...this.state.reviews, review]
+        })
+      }
+      this.setState({ loading: false})
+    } else {
+      window.alert('The forum contract could not be deployed to network')
+    }
+  }
+
+  addReview(genre, mname, rating, description) {
+    this.setState({ loading: true })
+    this.state.dmr.methods.addReview(genre, mname, rating, description).send({ from: this.state.account }).once('confirmation', (n, receipt) => {
+      this.setState({ loading: false })
+      window.location.reload()  
+    })
+  }
+
+  fundReview(id, fundrec) {
+    this.setState({ loading: true })
+    this.state.dmr.methods.fundReview(id).send({ from: this.state.account, value: fundrec })
+    .once('confirmation', (n, receipt) => {
+      this.setState({ loading: false })
+      window.location.reload()
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      dmr: null,
+      rCount: 0,
+      reviews: [],
+      loading: true
+    }
+    this.addReview = this.addReview.bind(this)
+    this.fundReview = this.fundReview.bind(this)
+  }
   
   render() {
     return (
       <Router>
         <Navbar />     
         <Route exact path="/" component={Home} />
+        <Route exact path="/reviews" render={props => (
+          <React.Fragment>
+            { this.state.loading
+            ? <center><br/><br/><br/><br/><br/><br/><div class="loader"></div></center>
+            : <Main
+              reviews={this.state.reviews}
+              addReview={this.addReview}
+              fundReview={this.fundReview}
+            />
+            }
+          </React.Fragment>
+        )} />
         <Footer />
       </Router>
     );
